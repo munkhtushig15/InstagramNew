90165eabc5ba3806ed338a52b0ed978159a8ea43
WiecznyX <bmunkhtushig00@gmail.com>
2022-12-04 15:05:13 +0800

    post duussan

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -10,25 +10,27 @@ const Posts = ({ post }) => {
           alt=""
           className="posterProfile"
         />
-        <span className="posterName">user name</span>
+        <span className="posterName">UserName</span>
       </div>
       <img
         src="https://pbs.twimg.com/media/EUj1VojXQAEL9Og.jpg"
         alt=""
         className="postPicture"
       />
-      <div className="icons">
+      <div className="postIconsContainer">
         <div className="sideIcons">
           <img
-          className="postIcons"
+            className="postIcons"
             src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
             alt="#"
           />
           <img
+            className="postIcons"
             src="https://cdn-icons-png.flaticon.com/512/2939/2939459.png"
             alt="#"
           />
           <img
+            className="postIcons"
             src="https://cdn-icons-png.flaticon.com/512/2099/2099085.png"
             alt="#"
           />
@@ -36,22 +38,27 @@ const Posts = ({ post }) => {
         <img
           src="https://cdn-icons-png.flaticon.com/512/5662/5662990.png"
           alt="#"
-          className="sideIcons"
+          className="sideIcons postIcons"
         />
       </div>
-      <span>{post.reactions} likes</span>
-      <div>
-        <span>user name</span>
+      <span className="postReactions">{post.reactions} likes</span>
+      <div className="postComment">
+        <span className="posterName">user name</span>
         <span>{post.title}</span>
       </div>
-      <div>
+      <div className="timeAgo">
         <span>{post.userId}</span>
         <span>MINUTES AGO</span>
       </div>
+      <hr />
       <div className="comments">
         <div className="commentLeft">
-          <img src="https://cdn-icons-png.flaticon.com/512/927/927567.png" alt="" />
-          <input type="text" placeholder="Add a comment ..." />
+          <img
+            className="postIcons"
+            src="https://cdn-icons-png.flaticon.com/512/927/927567.png"
+            alt=""
+          />
+          <input className="postCommentInput" type="text" placeholder="Add a comment ..." />
         </div>
         <span id="postColor">Post</span>
       </div>
--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,43 +1,55 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Card, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "../App.css";
+
 const Users = ({ user }) => {
   return (
-    <div>
-      <Container>
-        <Link
-          href="#"
-          style={{ color: "white" }}
-          className="text-decoration-none"
-          to={`/${user.id}`}
-        >
-          <Card
-            style={{
-              width: "120px",
-              backgroundColor: "darkwhite",
-              borderColor: "white",
-            }}
-          >
-            <Card.Body>
-              <Card.Title>
-                <img className="userProfile" src={user.image} alt="" />
+    // <div>
+    //   <Container>
+    //     <Link
+    //       href="#"
+    //       style={{ color: "white" }}
+    //       className="text-decoration-none"
+    //       to={`/${user.id}`}
+    //     >
+    //       <Card
+    //         style={{
+    //           width: "120px",
+    //           backgroundColor: "darkwhite",
+    //           borderColor: "white",
+    //         }}
+    //       >
+    //         <Card.Body>
+    //           <Card.Title>
+    //             <img className="userProfile" src={user.image} alt="" />
 
-                <center>
-                  <p
-                    style={{
-                      fontFamily: "Rockwell",
-                      color: "black ",
-                      marginTop: "15px",
-                    }}
-                  >
-                    {user.maidenName}
-                  </p>
-                </center>
-              </Card.Title>
-            </Card.Body>
-          </Card>
-        </Link>
-      </Container>
+    //             <center>
+    //               <p
+    //                 style={{
+    //                   fontFamily: "Rockwell",
+    //                   color: "black ",
+    //                   marginTop: "15px",
+    //                 }}
+    //               >
+    //                 {user.maidenName}
+    //               </p>
+    //             </center>
+    //           </Card.Title>
+    //         </Card.Body>
+    //       </Card>
+    //     </Link>
+    //   </Container>
+    // </div>
+    <div className="userContainer">
+      <Link
+        href="#"
+        style={{ color: "white" }}
+        className="LinkContainer"
+        to={`/${user.id}`}
+      >
+        <img className="userProfile" src={user.image} alt="" />
+        <span className="userMaidenName">{user.maidenName}</span>
+      </Link>
     </div>
   );
 };
